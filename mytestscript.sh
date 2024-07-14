17a28b22903b3bfde427d1450cc21befdf2752c4
Ravi Kukreja <38882529+kukrejaravi9@users.noreply.github.com>
2024-07-14 20:37:17 +0530

    Test script

---
+++ mytestscript.sh
@@ -0,0 +1,18 @@
+
+create_and_set_alias() {
+  local output_name=$1
+  local alias_url=$("https://$1")
+ 
+   echo "${output_name}=https://${alias_url}" >> "$GITHUB_OUTPUT"
+   echo "https://$alias_url"
+}
+
+main(){
+      alias_commit_jlg_id=$(create_and_set_alias "MY_CALL_ONE")
+      alias_commit_jdc_id=$(create_and_set_alias "MY_CALL_TWO")
+      call_three=$(create_and_set_alias "MY_CALL_THREE")
+      echo "DEV_JLG: $alias_commit_jlg_id" >> $GITHUB_STEP_SUMMARY
+      echo "DEV_JDC: $alias_commit_jdc_id" >> $GITHUB_STEP_SUMMARY
+      echo "CALL_THREE: $call_three" >> $GITHUB_STEP_SUMMARY
+}
+main
